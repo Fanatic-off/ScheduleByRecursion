1c493418b0b808430a8b1778ebe434ad5b7c2706
Eugene <Zhekul.90@gmail.com>
2021-05-04 18:49:09 +0300

    add MovieTheater CreateMovieGraph

--- MovieTheathetShowSchedule/MovieTheater.cs
+++ MovieTheathetShowSchedule/MovieTheater.cs
@@ -1,12 +1,69 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MovieTheathetShowSchedule
 {
-    class MovieTheater
+    public class MovieTheater
     {
+        public int TimeWork = (24 - 10) * 60;
+        public int Hall = 2;
+        public List<string> Movie;
+        public List<MovieTheater> Next;
+
+        public MovieTheater()
+        {
+
+        }
+
+        public void CreateMovieGraph(List<string> Movie, List<string> current)
+        {
+            if (current.Count == Hall)
+            {
+                foreach (string a in current)
+                {
+                    Console.Write(a + " ");
+                }
+                Console.WriteLine();
+            }
+            else
+            {
+                foreach (string a in Movie)
+                {
+                    List<string> temp = new List<string>();
+                    foreach (string b in current)
+                    {
+                        temp.Add(b);
+                    }
+                    temp.Add(a);
+                    CreateMovieGraph(Movie, temp);
+                }
+            }
+        }
+
+
+        //public MovieTheater()
+        //{
+        //    Next = new List<MovieTheater>();
+        //}
+
+        //public void CreateGraph()
+        //{
+        //    foreach(int hall in Hall)
+        //    {
+        //        //if (TimeMovie < TimeWork) { 
+        //        //}
+        //        List<int> temp = new List<int>();
+        //        foreach(int q in TimeMovie)
+        //        {
+        //            temp.Add(q);
+        //        }
+        //        temp.Add(hall);
+
+        //    }
+        //}
+
+
+
+
     }
 }
