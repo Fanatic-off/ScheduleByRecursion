e0d657613232533a4aad50433b0dea2ba736bf02
Eugene <Zhekul.90@gmail.com>
2021-05-05 17:32:44 +0300

    add MovieTheater  CreateMovieGraph

--- MovieTheathetShowSchedule/MovieTheater.cs
+++ MovieTheathetShowSchedule/MovieTheater.cs
@@ -8,14 +8,15 @@ namespace MovieTheaterShowSchedule
         public int TimeWork = (24 - 10) * 60;
         public int Hall = 2;
         public int TimeMovie = 90;
-        public List<string> Movie;
+        public static List<string> Movie;
         public List<string> CurrentMovie;
+        public List<MovieTheater> Next;
 
-        public MovieTheater(int timeWork, List<string> current=null)
+        public MovieTheater(int timeWork = (24 - 10) * 60, List<string> current = null)
         {
             TimeWork = timeWork;
 
-            if (current!= null)
+            if (current != null)
             {
                 CurrentMovie = current;
             }
@@ -23,31 +24,49 @@ namespace MovieTheaterShowSchedule
             {
                 CurrentMovie = new List<string>();
             }
+
+            Next = new List<MovieTheater>();
         }
 
-        public void CreateMovieGraph(List<string> Movie, List<string> current)
+        public void CreateMovieGraph()
         {
-            if (current.Count == Hall)
+            foreach (string movie in Movie)
             {
-                foreach (string a in current)
-                {
-                    Console.Write(a + " ");
-                }
-                Console.WriteLine();
-            }
-            else
-            {
-                foreach (string a in Movie)
+                if (TimeWork - TimeMovie > 0)
                 {
                     List<string> temp = new List<string>();
-                    foreach (string b in current)
+                    foreach (string m in CurrentMovie)
                     {
-                        temp.Add(b);
+                        temp.Add(m);
                     }
-                    temp.Add(a);
-                    CreateMovieGraph(Movie, temp);
+                    temp.Add(movie);
+                    MovieTheater theater = new MovieTheater(TimeWork - TimeMovie, temp);
+
+                    Next.Add(theater);
+                    theater.CreateMovieGraph();
                 }
             }
+            //if (current.Count == Hall)
+            //{
+            //    foreach (string a in current)
+            //    {
+            //        Console.Write(a + " ");
+            //    }
+            //    Console.WriteLine();
+            //}
+            //else
+            //{
+            //    foreach (string a in Movie)
+            //    {
+            //        List<string> temp = new List<string>();
+            //        foreach (string b in current)
+            //        {
+            //            temp.Add(b);
+            //        }
+            //        temp.Add(a);
+            //        CreateMovieGraph(Movie, temp);
+            //    }
+            //}
         }
     }
 }
