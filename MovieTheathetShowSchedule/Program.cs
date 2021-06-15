c503ba6d3dd7489132d8a58b6357bde5f2c4a968
Eugene <Zhekul.90@gmail.com>
2021-06-15 15:35:14 +0300

    Fix format

--- MovieTheathetShowSchedule/Program.cs
+++ MovieTheathetShowSchedule/Program.cs
@@ -1,5 +1,4 @@
-﻿using MovieTheaterShowSchedule;
-using System;
+﻿using System;
 using System.Collections.Generic;
 
 namespace MovieTheaterShowSchedule
@@ -10,17 +9,20 @@ namespace MovieTheaterShowSchedule
         {
             MovieTheater theater = new MovieTheater(500);
             MovieTheater.FilmsTime = new List<int>() { 90, 120, 60 };
+
             theater.CreateMovieGraph();
             theater.WriteLeavesGraph();
 
             Schedule schedule = theater.GetResult();
+
+            Console.WriteLine();
+            Console.Write($"Лучший вариант - ");
             foreach (int w in schedule.CurrentMovie)
             {
                 Console.Write(w + " ");
             }
             Console.WriteLine();
             Console.WriteLine($"Свободного времени осталось {schedule.TimeWork}");
-
         }
     }
 }
